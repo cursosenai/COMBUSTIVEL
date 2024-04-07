052810895c6c58c041931c8cb8bb485092acb1ef
cursosenai <166341518+cursosenai@users.noreply.github.com>
2024-04-07 20:19:44 -0300

    COMBUSTIVEL.ino

--- COMBUSTIVEL.ino
+++ COMBUSTIVEL.ino
@@ -1,17 +1,17 @@
 /*                                                SENAI CAXIAS
  *                                     ANALIZADOR DE COMBUSTIVEL ADULTERADO
- *                                                11/04/2024
- *                                              	ALUNOS
+ *                                                18/04/2024
+ *                                                  ALUNOS
  *                                  ALEXANDRE, DANIEL, SILVANIA, LUCAS, BRENDA 
  */
 
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>		
-									//Inicializa o display no endereco 0x27
-LiquidCrystal_I2C lcd(0x27,16,4);
+					
+LiquidCrystal_I2C lcd(0x27,16,4);	//Inicializa o display no endereco 0x27
 
-									//int pin = 10;
+					//int pin = 10;
 int analyzer=0, comb1=0;
 volatile int state = LOW;
 volatile int state1 = LOW;
@@ -21,17 +21,17 @@ analyzer++;
 } 
 void setup()
 {
-									//pinMode(pin, OUTPUT);
+					//pinMode(pin, OUTPUT);
 attachInterrupt(0, analy, FALLING);
- lcd.init();						//inicia LCD de 16 colunas 4 linhas
+ lcd.init();				//inicia LCD de 16 colunas 4 linhas
  lcd.setBacklight(HIGH);
- lcd.setCursor(0,0);				//posiciona cursor na linha 1 coluna 2
+ lcd.setCursor(0,0);			//posiciona cursor na linha 1 coluna 2
  lcd.print("SENAI CAXIAS/RJ");		//escreve no lcd na posição acima
- lcd.setCursor(0,1);				//posiciona cursor na linha 1 coluna 2
+ lcd.setCursor(0,1);			//posiciona cursor na linha 1 coluna 2
  lcd.print("AUT. INDUSTRIAL");		//escreve no lcd na posição acima
  delay(4000);
  lcd.clear();
-									// inicializa timer1 
+					// inicializa timer1 
   noInterrupts();           		// desabilita todas as interrupções
   TCCR1A = 0;
   TCCR1B = 0;
@@ -56,21 +56,21 @@ ISR(TIMER1_COMPA_vect)          	// timer compare interrupt service routine
 
 void loop()
 {
- lcd.setCursor(0,0);				//posiciona cursor na linha 1 coluna 2
+ lcd.setCursor(0,0);			//posiciona cursor na linha 1 coluna 2
  lcd.print("PERCENTUAL DE");		//escreve no lcd na posição acima
-  lcd.setCursor(0,1);				//posiciona cursor na linha 1 coluna 1
- lcd.print("ETANOL= ");				//escreve no lcd na posição acima
+  lcd.setCursor(0,1);			//posiciona cursor na linha 1 coluna 1
+ lcd.print("ETANOL= ");			//escreve no lcd na posição acima
  if(comb1>2){
- lcd.print(comb1);					//escreve no lcd na posição acima 
-lcd.print("%       ");				//escreve no lcd na posição acima  
+ lcd.print(comb1);			//escreve no lcd na posição acima 
+lcd.print("%       ");			//escreve no lcd na posição acima  
  }else{
-  lcd.print("S/DADOS");				//escreve no lcd na posição acima 
+  lcd.print("S/DADOS");			//escreve no lcd na posição acima 
  }
  if(comb1>100){
-  lcd.setCursor(4,2);				//posiciona cursor na linha 1 coluna 2
+  lcd.setCursor(4,2);			//posiciona cursor na linha 1 coluna 2
   lcd.print("ERROR");
  }else{
-   lcd.setCursor(4,2);				//posiciona cursor na linha 1 coluna 2
+   lcd.setCursor(4,2);			//posiciona cursor na linha 1 coluna 2
  lcd.print("        ");
 }
 }
